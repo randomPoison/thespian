226378d5737768df9fe1e202f3150e3b956cf03f
David LeGare <dlegare.1001@gmail.com>
2020-01-31 18:29:08 -0600

    More robust support for patterns in actor method arguments (#5)

--- tests/derive.rs
+++ tests/derive.rs
@@ -19,6 +19,15 @@ impl MyActor {
     pub fn multiple_params(&self, _first: usize, _second: String) {
         unimplemented!()
     }
+
+    pub fn mut_param(&self, mut some_param: String) -> String {
+        some_param.push_str(" and some more stuff");
+        some_param
+    }
+
+    pub fn pat_param_tuple(&self, (foo, bar): (usize, String)) {
+        dbg!(foo, bar);
+    }
 }
 
 // Test that the derive works with a second impl block.
